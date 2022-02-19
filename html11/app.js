05e27c5de11c3a551f6507cd995f48e64d52d9f6
Eoin Carrick <22carri99@gmail.com>
2022-02-19 19:21:50 +0000

    code

--- html11/app.js
+++ html11/app.js
@@ -107,18 +107,67 @@
 // prev.onclick = function () {
 //   img.setAttribute("src", images[decrementIndex(currentIndex)]);
 // };
+const ul = document.querySelector("ul");
 
-const createTodo = () => {
-  //Creating our HTML element
-  const li = document.createElement("li");
-  const input = document.createElement("input");
-  const label = document.createElement("label");
-  const button = document.createElement("button");
+// const createTodo = () => {
+//   //Creating our HTML element
+//   const li = document.createElement("li");
+//   const input = document.createElement("input");
+//   const label = document.createElement("label");
+//   const button = document.createElement("button");
 
-  //Setting the attribute of the HTMl element
-  input.setAttribute("type", "checkbox");
-  console.log(input)
-  li.appendChild(input);
-  console.log(li);
+//   //Setting the attribute of the HTMl element
+//   input.setAttribute("type", "checkbox");
+//   label.innerHTML = "Learn CSS";
+//   button.setAttribute("class", "delete");
+//   button.innerHTML = "Delete";
+
+//   //Appending the input,label and the button to li
+//   li.appendChild(input);
+//   li.appendChild(label);
+//   li.appendChild(button);
+//   return li;
+// };
+// console.log(createTodo());
+var toggleCheck = function () {
+  let parent = this.parentNode;
+  parent.classList.toggle("checked");
+};
+var deleteTodo = function () {
+  let todoLi = this.parentNode;
+  todoLi.remove();
+};
+var createTodo = function (todo) {
+  let li = document.createElement("li");
+
+  let label = document.createElement("label");
+  label.innerHTML = todo;
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.onclick = toggleCheck;
+
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "Delete";
+  deleteButton.className = "delete";
+  deleteButton.onclick = deleteTodo; //nest todo elements in list item
+
+  li.appendChild(checkbox);
+  li.appendChild(label);
+  li.appendChild(deleteButton);
+  return li;
 };
 createTodo();
+
+document.getElementById("add").onclick = function () {
+  // store the button's parent element (.addTodo <div>) in a variable
+  var parent = this.parentNode;
+  // store the input, which is the *first* child element of the .addTodo <div>
+  var addTextInput = parent.children[0];
+  if (addTextInput.value === "") {
+    return;
+  } else {
+    //add todo
+    createTodo(addTextInput.value); //reset input
+    addTextInput.value = "";
+  }
+};
