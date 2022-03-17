d57e7d2e01c61731707f5f31b679ac04367f526a
Eoin Carrick <22carri99@gmail.com>
2022-03-17 16:01:21 +0000

    save

--- html20/app.js
+++ html20/app.js
@@ -5,6 +5,8 @@ const things = require("./things.js");
 // the app.get(takes a route in '/' or '/route_name', then takes a callback function())
 app.use(things, things);
 
-app.get("/*");
+app.get("*", function (req, res) {
+  res.send("Sorry this page cannot be found");
+});
 
 app.listen(3090);
