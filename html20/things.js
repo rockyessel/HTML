b1593ff98123c0bba8c758e19ef99b1757dd0c4e
Eoin Carrick <22carri99@gmail.com>
2022-03-17 16:01:01 +0000

    code

--- html20/things.js
+++ html20/things.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/get", (req, res) => {
   res.send("THIS IS THE GET METHOD");
 });
 
-router.post("/post", (req, res) => {
-  res.send("THIS IS THE POST METHOD");
+router.post("/:name", (req, res) => {
+  res.send("The id you specified is " + req.params.id);
 });
 
 module.exports = router;
