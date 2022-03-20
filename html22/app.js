ad26d4ea4b9adc4acff70abbb62a603602a2d8e5
Eoin Carrick <22carri99@gmail.com>
2022-03-20 22:08:10 +0000

    did it my won

---
+++ html22/app.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector('.dropdown_btn');
+
+const toggle = () => {
+  document.querySelector('.content').classList.toggle('show');
+};
+btn.addEventListener('click', toggle);
+
+const filterFunction = () => {
+  const input = document.querySelector('[type=text]');
+  const filter = input.value.toLowerCase();
+  const content_div = document.querySelector('.content');
+  const a = content_div.getElementsByTagName('a');
+  let inputTextValue;
+  for (let i = 0; i < a.length; i++) {
+    inputTextValue = a[i].innerText || a[i].textContent;
+    if (inputTextValue.toLowerCase().indexOf(filter) > -1) {
+      a[i].style.display = '';
+    } else {
+      a[i].style.display = 'none';
+    }
+  }
+};
