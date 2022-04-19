86699a60192b2552f3cb528a23d9a3afcc5ab749
Eoin Carrick <22carri99@gmail.com>
2022-04-19 23:06:08 +0000

    saev

--- html25/index.js
+++ html25/index.js
@@ -10,31 +10,97 @@ app.get('/', (request, response) => {
   });
 });
 
-const container = [];
+// Quotes
+const quoteNumbers = [];
+for (let i = 1; i <= 100; i++) {
+  quoteNumbers.push(i);
+}
+const quoteRandomNumber = Math.floor(
+  Math.random() * quoteNumbers.length
+).toString();
 
-axios
-  .get('https://www.hellenicshippingnews.com/tag/top-stories/')
-  .then((res) => {
-    const html = res.data;
-    const $ = cheerio.load(html);
-    // console.log(html);
+// Author
+const authorNumbers = [];
+for (let i = 1; i <= 20; i++) {
+  authorNumbers.push(i);
+}
+const authorRandomNumber = Math.floor(
+  Math.random() * authorNumbers.length
+).toString();
 
-    $(`a:contains("shipping")`, html).each(function () {
-      const text = $(this).text();
-      const title = text.trim();
-      const url = $(this).attr('href');
-      container.push({
-        title,
-        url,
-      });
+const url = {
+  quotes: `https://quotepark.com/quotes/recently-liked/?page=${quoteRandomNumber}`,
+  author: `https://quotepark.com/authors/?page=${authorRandomNumber}`,
+};
+
+//Quote
+axios.get(url.quotes).then((res) => {
+  const html = res.data;
+  const $ = cheerio.load(html);
+
+  $('.quote-body', html).each(function () {
+    const text = $(this).find('.blockquote-text').find('a').text();
+    const quoteContent = text.trim();
+    const quoteSource = $(this)
+      .find('.blockquote')
+      .find('.blockquote-text')
+      .find('a')
+      .attr('href');
+    const authorThumbnail = $(this)
+      .find('.quote-thumbnail')
+      .find('.lazy')
+      .attr('data-src');
+    const authorLink = $(this).find('.quote-thumbnail').attr('href');
+    const authorName = $(this)
+      .find('.blockquote')
+      .find('.blockquote-origin')
+      .find('a')
+      .text();
+    const quoteTags = {
+      link: $(this).find('._gtm_quote_tag').attr('href'),
+      tags: $(this).find('._gtm_quote_tag').text().trim().split(' '),
+    };
+    quotes.push({
+      quoteContent,
+      quoteSource,
+      quoteTags,
+      authorLink,
+      authorName,
+      authorThumbnail,
     });
   });
+});
+const quotes = [];
+app.get('/quotes', (request, response) => {
+  response.json(quotes);
+});
 
-app.get('/shipping-news', (request, response) => {
-  response.json(container);
+//Author
+axios.get(url.author).then((res) => {
+  const html = res.data;
+  const $ = cheerio.load(html);
+
+  $('.media-author', html).each(function () {
+    const name = $(this)
+      .find('.media-body')
+      .find('.media-author-name')
+      .find('a')
+      .text();
+    const image = $(this)
+      .find('.media-author-img')
+      .find('img')
+      .attr('data-src');
+    author.push({
+      name,
+      image,
+    });
+  });
+});
+const author = [];
+app.get('/author', (request, response) => {
+  response.json(author);
 });
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
- 
