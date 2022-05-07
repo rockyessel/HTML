c2f063fa0694a370d3e28f582ab3bcd0d1617656
Eoin Carrick <22carri99@gmail.com>
2022-05-07 22:34:22 +0000

    kkk

--- html14/app.js
+++ html14/app.js
@@ -16,28 +16,3 @@ const fetchData = async (dhlURL) => {
   console.log('DHL', data);
 };
 fetchData(dhlURL);
-//
-
-// FedEx API INTEGRATION
-
-// 'input' refers to JSON Payload
-var data = JSON.stringify({
-  name: 'ewerwdwsdwed',
-});
-
-var xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-
-xhr.addEventListener('readystatechange', function() {
-  if (this.readyState === 4) {
-    console.log(this.responseText);
-  }
-});
-
-xhr.open('POST', 'https://apis-sandbox.fedex.com/track/v1/associatedshipments');
-xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.setRequestHeader('X-locale', 'en_US');
-xhr.setRequestHeader('Authorization', 'Bearer ');
-
-xhr.send(data);
-//
